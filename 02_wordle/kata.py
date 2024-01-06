13ebca25e45bbc18972f32161a4ac89f71536642
Samuele Mattiuzzo <samuele.mattiuzzo@digital.trade.gov.uk>
2024-01-06 06:39:01 +0000

    Wordle kata

--- 02_wordle/kata.py
+++ 02_wordle/kata.py
@@ -2,7 +2,24 @@ class WordleChecker:
 
     def __init__(self, *args, **kwargs):
         super(WordleChecker, self).__init__(*args, **kwargs)
-        self.result = "0"
+        self.result = ''
 
     def get_result(self):
         return self.result
+
+    def check_wordle(self, target, guess):
+        tmp_result = ''
+
+        for g_idx, g_symbol in enumerate(guess):
+            if g_symbol in target:
+                if target[g_idx] != g_symbol:
+                    tmp_result += '1'
+                else:
+                    tmp_result += '2'
+            else:
+                tmp_result += '0'
+
+        if not len(tmp_result):
+            tmp_result = '0'
+
+        self.result = tmp_result
--- 02_wordle/test.py
+++ 02_wordle/test.py
@@ -9,9 +9,21 @@ class WordleTest(unittest.TestCase):
         self.test_wordle = WordleChecker()
 
     def test_checker_returns_0_if_guess_not_in_target(self):
+        self.test_wordle.check_wordle("a", "b")
         result = self.test_wordle.get_result()
         self.assertEqual(result, "0")
 
     def test_checker_returns_1_if_guess_in_target_not_in_position(self):
+        self.test_wordle.check_wordle("ab", "ba")
         result = self.test_wordle.get_result()
-        self.assertEqual(result, "0")
+        self.assertEqual(result, "11")
+
+    def test_checker_returns_2_if_guess_in_target_in_position(self):
+        self.test_wordle.check_wordle("a", "a")
+        result = self.test_wordle.get_result()
+        self.assertEqual(result, "2")
+
+    def test_checker_returns_full_sequence_if_guess_in_target_in_position(self):
+        self.test_wordle.check_wordle("abcaa", "acbax")
+        result = self.test_wordle.get_result()
+        self.assertEqual(result, "21120")
